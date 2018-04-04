973a6086b9a78aefd268e17d36ea92054885fc86
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-04-04 14:59:40 -0600

    project initialization

---
+++ src/main/java/com/chaseclinecoding/middleearthapi/MiddleEarthApiApplication.java
@@ -0,0 +1,12 @@
+package com.chaseclinecoding.middleearthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleEarthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleEarthApiApplication.class, args);
+	}
+}
